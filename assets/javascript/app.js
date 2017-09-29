e4b43ac3fce0733df2017191464cc1333b237322
Minu James <minujames@minus-mbp.gateway.pace.com>
2017-09-28 21:04:40 -0700

    adding real questions and images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,18 +25,35 @@ $(document).ready(function(){
     winsCount: 0,
 
     initialize: function(){
-      var question1 = new Question("1-What is the color of the sky?", 
-        ["red", "blue", "green", "yellow"], "blue", "");
-      var question2 = new Question("2-What is the color of the sky?", 
-        ["red", "blue", "green", "yellow"], "blue", "");
-      var question3 = new Question("3-What is the color of the sky?", 
-        ["red", "blue", "green", "yellow"], "blue", "");
-      var question4 = new Question("4-What is the color of the sky?", 
-        ["red", "blue", "green", "yellow"], "blue", "");
-      var question5 = new Question("5-What is the color of the sky?", 
-        ["red", "blue", "green", "yellow"], "blue", "");
 
-      triviaGame.questions.push(question1, question2, question3, question4, question5);
+      var question1 = new Question("1. What was the first national park in America?", 
+        ["Death Valley National Park", "Yosemite National Park", "Acadia National Park", 
+        "Yellowstone National Park"], "Yellowstone National Park", "yellow-stone-np.jpg");
+      
+      var question2 = new Question("2. What national park is home to the world's largest tree by volume?", 
+        ["Redwood National Park", "Sequoia National Park", "Everglades National Park", 
+        "Joshua Tree National Park"], "Sequoia National Park", "sequoia-np.jpg");
+
+      var question3 = new Question("3. Which state contains the most national parks?", 
+        ["Colorado", "Utah", "Alaska", "California"], "California", "yosemite-np.jpg");
+
+      var question4 = new Question("4. Which is the most visited national park?", 
+        ["Yosemite National Park", "Grand Canyon National Park", "Great Smoky Mountains National Park", 
+        "Rocky Mountain National Park"], "Great Smoky Mountains National Park", "great-smoky-mountains-np.jpg");
+     
+      var question5 = new Question("5. Who is considered as The Father of the National Parks?",
+        ["John Muir", "Theodore Roosevelt", "Joseph Le Conte", "Ralph Waldo Emerson"], 
+        "John Muir", "john_muir.jpg");
+
+      var question6 = new Question("6. Which national park is home to the deepest lake in the U.S?", 
+        ["Lake Clark National Park", "Kenai Fjords National Park", "Crater Lake National Park", 
+        "Great Basin National Park"], "Crater Lake National Park", "crater-lake-np.jpg");
+
+      var question7 = new Question("7. Which National Park contains the highest peak in North America?", 
+        ["Grand Teton National Park", "Hawaii Volcanoes National Park", "Mount Rainier National Park", 
+        "Denali National Park"], "Denali National Park", "denali-np.jpg");
+
+      triviaGame.questions.push(question1, question2, question3, question4, question5, question6, question7);
 
       triviaGame.reset();
     },
@@ -78,16 +95,19 @@ $(document).ready(function(){
     },
 
     displayQuestion: function(question){
-      console.log("inside display question");
-      $("#question-div").html(triviaGame.currentQuestionObj.question);
+      $("#question-div").text(triviaGame.currentQuestionObj.question);
 
       var optionsDiv = $("#options-div");
+      var newDiv = $('<div>');
+
       var optionsArray = triviaGame.currentQuestionObj.options;
+
       optionsArray.forEach(function(option){
-        console.log(option);
-        $('<p><input class="radio-btn" type="radio" name="option" value=' + option + '>' +
-          option + '</input></p>').appendTo(optionsDiv);
+        newDiv.append( $('<p><input type="radio" name="option" value="' + option + 
+          '"></input><label class="option-label">' + option + '</label></p>'));
       });
+
+      optionsDiv.html(newDiv);
     },
 
     evaluateAnswer: function(selectedAnswer){
@@ -157,11 +177,3 @@ $(document).ready(function(){
   });
 
 });
-
-
-
-
-// for (i = 0; i < 4; i++) {
-//     $('<input class="radio-button" type="radio" name="dynradio" value="hello click">hello</input>')
-//     .appendTo('.options');
-// }
